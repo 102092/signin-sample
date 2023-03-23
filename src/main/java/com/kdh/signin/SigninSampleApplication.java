0890feaea0332aed99794d4839739449e59ffbe8
102092 <quaint3@gmail.com>
2023-03-23 12:26:35 +0900

    Initial commit

---
+++ src/main/java/com/kdh/signin/SigninSampleApplication.java
@@ -0,0 +1,13 @@
+package com.kdh.signin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigninSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SigninSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kdh/signin/SigninSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdh.signin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigninSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
